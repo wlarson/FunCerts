74da1f1d22757561a341d892ab4f433b8e8e1dcd
Will Larson <wlarson@gmail.com>
2017-02-28 16:26:41 -0800

    fix for searching via tag cloud

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :taggings
-  resources :tags
+#  resources :taggings
+#  resources :tags
   resources :certifications
+  
+  get 'tags/:tag', to: 'certifications#index', as: :tag, :constraints  => { :tag => /[^\/]+/ }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'certifications#index'
